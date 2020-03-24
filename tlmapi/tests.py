2a1c2c31c390ea4e46df0c67d6a5226e73714dcf
TPPonmat <nott_showba@hotmail.com>
2020-03-24 15:14:57 +0700

    add dev test

--- tlmapi/tests.py
+++ tlmapi/tests.py
@@ -7,8 +7,10 @@ pst_now = utc_now.astimezone(pytz.timezone('Asia/Bangkok'))
 thaidatetime = pst_now.strftime("%Y-%m-%d %H:%M:%S")
 thaidatetime2 = pst_now.strftime("%X")
 
-print (pst_now.date())
+print (type(int(pst_now.time().strftime("%H"))))
 
+import random
+print (round((random.random()*200000)+20000,2))
 
 # utc_now_time = pytz.utc.localize()
 
--- tlmapi/urls.py
+++ tlmapi/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('api/sampleapi', views.sample_api),
     path('api/auto_record', views.auto_record),
+    path('api/record', views.record),
+    path('api/gettestrecord', views.gettestrecord),
     path('api/gettlm', views.gettlm),
     # path('api/line', views.line)
 ]
